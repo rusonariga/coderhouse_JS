d4df7e2d320a412ea30e1dda351a53ef12b0f1d7
rusonariga <gmimbrioscia@gmail.com>
2021-06-12 00:04:00 -0300

    final push

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,3 +1,7 @@
+const urlProducts = "https://my-json-server.typicode.com/rusonariga/coderhouse_JS/Products";
+const urlPromos = "https://my-json-server.typicode.com/rusonariga/coderhouse_JS/Promos";
+var Products=[], subtotal=0, Promos=[], shipCost=0, promoValue=0, total=0;
+
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
   'use strict'
@@ -13,8 +17,194 @@
           event.preventDefault()
           event.stopPropagation()
         }
+        else {
+          event.preventDefault();
+          $.get(urlProducts)
+          .done((serverData)=>{
+              
+              let serverDataIndex=0;
+              for (product of Products) {
+                  serverDataIndex=serverData.findIndex((dataProduct)=> {return dataProduct.id===product.id});
+                  if (serverDataIndex==-1) {
+                      alert(`El producto ${product.name} que intentaba comprar ha sido eliminado del catálogo. Le llevaremos de vuelta a la sección de compras`);
+                      break;
+                  }
+                  else if (product.amountToBuy>serverData[serverDataIndex].stock) {
+                      alert(`Lo sentimos, quedan ${product.stock} unidades disponibles de ${product.name}. Le llevaremos de vuelta a la sección de compras`);
+                      serverDataIndex=-1;
+                      break;
+        
+                  }
+              }
+              if (serverDataIndex==-1) window.setTimeout(function(){window.location.href="imbrioscia_cart.html"}, 3000);
+              else {
+                alert("Su compra se realizó con éxito. ¿Desea seguir comprando? Le llevaremos de vuelta a la sección de compras");
+                window.setTimeout(function(){window.location.href="imbrioscia_cart.html"}, 1000);
+              }
+          });
+        }
         form.classList.add('was-validated')
       }, false)
     })
   }, false)
 }())
+
+$(document).ready( ()=> {
+  Products=JSON.parse(sessionStorage.getItem("prodPurchase"));
+  if (Products==null) {
+    alert("No recibimos información de su compra. Le redirigiremos a la ventana de selección de productos");
+    window.setTimeout(function(){window.location.href="imbrioscia_cart.html"}, 3000);
+  }
+  else {
+    $("#prodQty").text(Products.length);
+    for(product of Products){
+      listNewRow(product);
+      subtotal+=product.amountToBuy*product.price;
+    }
+    subtotalRow();
+    shipmentRow();
+    $.get(urlPromos).done(function(respuesta) {
+      Promos=respuesta;
+      $("#promoInput").show();
+      promoRow();
+    })
+    .always(()=>{totalRow();});
+    
+
+    
+  }
+
+
+
+})
+
+listNewRow = (product) => {
+  let listRowHTML =
+  `<li class="list-group-item d-flex justify-content-between lh-condensed">
+    <div>
+      <h6 class="my-0" id="prod${product.id}Name">${product.name}</h6>
+      <small class="text-muted" id="prod${product.id}AmountToBuy">${product.amountToBuy} unidades</small>
+    </div>
+    <span class="text-muted" id="prod${product.id}TotalPrice">${pesoFormat(product.price*product.amountToBuy)}</span>
+  </li>`;
+  // `<tr id="prod${product.id}CartRow">
+  //     <td class="text-left">${product.name}</td>
+  //     <td class="text-center" id="prod${product.id}AmountToBuy">0</td>
+  //     <td class="text-right"><button class="btn btn-sm btn-danger" onclick="removeFromCart(${product.id})"><i class="fa fa-trash"></i></button></td>
+  // </tr>`;
+
+  $("#cartProdList").append(listRowHTML);
+}
+
+subtotalRow = () => {
+  subtotalRowHTML=
+    `<li class="list-group-item d-flex justify-content-between">
+        <span>Subtotal</span>
+        <strong id="subtotal">${pesoFormat(subtotal)}</strong>
+    </li>`;
+    $("#cartProdList").append(subtotalRowHTML);
+
+}
+
+shipmentRow = () => {
+  shipCost=Math.max(subtotal*0.1,125);
+  shipmentRowHTML=
+    `<li class="list-group-item d-flex justify-content-between">
+        <span>Costo de envío</span>
+        <span id="shipment">${pesoFormat(shipCost)}</span>
+    </li>`;
+    $("#cartProdList").append(shipmentRowHTML);
+
+}
+
+promoRow = () => {
+  promoRowHTML=
+    `<li class="list-group-item d-flex justify-content-between bg-light">
+        <div class="text-success">
+          <h6 class="my-0" id="promoCode">Código de promo</h6>
+          <small id="promoDesciption">Introduzca un código de promoción</small>
+        </div>
+        <span class="text-success" id="promo">$--</span>
+      </li>`;
+    $("#cartProdList").append(promoRowHTML);
+
+}
+
+totalRow = () => {
+  total=shipCost+subtotal;
+  totalRowHTML=
+    `<li class="list-group-item d-flex justify-content-between light-gray bg-success">
+        <strong>Total</strong>
+        <strong id="total">${pesoFormat(total)}</strong>
+    </li>`;
+    $("#cartProdList").append(totalRowHTML);
+
+}
+
+pesoFormat = (number) => {
+  let numberPesos= new Intl.NumberFormat("es-419",{style:"currency",currency:"ARS",currencyDisplay:"narrowSymbol",minimumFractionDigits:"2"}).format(number);
+  return numberPesos;
+}
+
+promoButton = (e) => {
+  e.preventDefault();
+  let promoEntered=$("#promoText").val();
+  let promoIndex=Promos.findIndex((promo)=> {return promo.code===promoEntered});
+  if (promoIndex==-1) {
+    alert("Código de promoción inválido");
+    $("#promoText").val("");
+    return;
+  }
+  
+  switch (Promos[promoIndex].type) {
+    case "discount value":
+      promoValue=Promos[promoIndex].value;
+      if (subtotal<=2*promoValue) {
+        alert("Promoción no aplicable a esta compra. La compra debe ser de un valor mínimo de "+pesoFormat(2*promoValue));
+      }
+      else {
+        total=subtotal+shipCost-promoValue;
+        $("#total").text(pesoFormat(total));
+        $("#promo").text(pesoFormat(-promoValue));
+        $("#promoCode").text(promoEntered);
+        $("#promoDesciption").text("Descuento");
+      }
+      $("#promoText").val("");
+      break;
+    
+    case "discount percentage":
+      promoValue=Promos[promoIndex].value*subtotal/100;
+      total=subtotal+shipCost-promoValue;
+      $("#total").text(pesoFormat(total));
+      $("#promo").text(pesoFormat(-promoValue));
+      $("#promoCode").text(promoEntered);
+      $("#promoDesciption").text("Descuento del "+Promos[promoIndex].value+"% sobre subtotal");
+      $("#promoText").val("");
+      break;
+
+    case "free shipment":
+      promoValue=shipCost;
+      total=subtotal+shipCost-promoValue;
+      $("#total").text(pesoFormat(total));
+      $("#promo").text(pesoFormat(-promoValue));
+      $("#promoCode").text(promoEntered);
+      $("#promoDesciption").text("Envío gratis");
+      $("#promoText").val("");
+      break;
+    case "shipment percentage":
+      promoValue=shipCost*Promos[promoIndex].value/100;
+      total=subtotal+shipCost-promoValue;
+      $("#total").text(pesoFormat(total));
+      $("#promo").text(pesoFormat(-promoValue));
+      $("#promoCode").text(promoEntered);
+      $("#promoDesciption").text("Descuento del "+Promos[promoIndex].value+"% sobre el envío");
+      $("#promoText").val("");
+      break;
+    }
+  $("#promoText").val("");
+}
+
+buyButton = (e) => {
+  e.preventDefault();
+  
+}
--- js/imbrioscia_cart.js
+++ js/imbrioscia_cart.js
@@ -4,54 +4,49 @@ class Producto {
         this.name = name;
         this.price = price;
         this.stock = stock;
-    }
-
-    getStock() {
-        return this.stock;
+        this.amountToBuy=0;
     }
 }
 
-var Productos = [
-                new Producto(1, "Producto 1", 124.90, 0),
-                new Producto(2, "Producto 2", 33.90, 15),
-                new Producto(3, "Producto 3", 70.00, 17),
-                new Producto(4, "Papas", 7988, 200)
-            ];
+var Products = [];
 
-var cartProducts = [];
+
+const urlProducts = "https://my-json-server.typicode.com/rusonariga/coderhouse_JS/Products";
+
+
+
 
 $(document).ready( ()=> {
-    for(producto of Productos){
-        prodNewRow(producto);
-        cartNewRow(producto);
-        cartProducts.push(0);
-        // if (producto.stock!=0) {
-        //     $("#prod"+producto.id+"Stock").text("En stock");
-        //     $("#prod"+producto.id+"AddButton").prop("disabled",false);
-        //     $("#prod"+producto.id+"Input").prop("max",producto.stock).prop("disabled",false);
-        // }
-        // else $("#prod"+producto.id+"Stock").text("Sin stock");
+    $.get(urlProducts).done(function(respuesta) {
+            Products=respuesta;
+            for(product of Products){
+                product.amountToBuy=0;
+                prodNewRow(product);
+                cartNewRow(product);
+            }
+            $("#prodTableBody td").addClass("align-middle");
 
-        // $("#prod"+producto.id+"Price").text(pesoFormat(producto.price));
-    }
-    $("#prodTableBody td").addClass("align-middle");
+    }).fail(()=>{
+        alert("Ha ocurrido un error al cargar los productos de nuestro servidor. Sepa disculpar las molestias");
+    }).always(()=>{});
+
 });
 
-function prodNewRow(producto){
-    if (producto.stock==0) {
+function prodNewRow(product){
+    if (product.stock==0) {
         stockCheck="Sin stock";
         prodEnable="disabled";}
     else {
         stockCheck="En stock";
         prodEnable="";}
     prodRowHTML =
-    `<tr>
-        <td> <img src="https://dummyimage.com/50x50/55595c/fff"/> </td>
-        <td> ${producto.name} </td>
-        <td id="prod${producto.id}Stock">${stockCheck}</td>
-        <td class="text-center"><input id="prod${producto.id}Input" ${prodEnable} type="number" step="1" max="${producto.stock}" min="1" value="1" title="Qty" class="qty" size="4"></td>
-        <td class="text-right" id="prod${producto.id}Price">${pesoFormat(producto.price)}</td>
-        <td><button class="btn btn-sm btn-success" id="prod${producto.id}AddButton" onclick="addToCart(${producto.id})" ${prodEnable}><i class="fa fa-plus"></i></button></td>
+    `<tr id="prod${product.id}Row">
+        <td class="d-none d-sm-table-cell"> <img src="https://dummyimage.com/50x50/55595c/fff"/> </td>
+        <td> ${product.name} </td>
+        <td  class="d-none d-sm-table-cell" id="prod${product.id}Stock">${stockCheck}</td>
+        <td class="text-center"><input id="prod${product.id}Input" ${prodEnable} type="number" step="1" max="${product.stock}" min="1" value="1" title="Qty" class="qty" size="3"></td>
+        <td class="text-right" id="prod${product.id}Price">${pesoFormat(product.price)}</td>
+        <td><button class="btn btn-sm btn-success" id="prod${product.id}AddButton" onclick="addToCart(${product.id})" ${prodEnable}><i class="fa fa-plus"></i></button></td>
     </tr>`;
 
     $("#prodTableBody").append(prodRowHTML);
@@ -62,156 +57,57 @@ function workInProgress() {
     alert(message);
 }
 
-function addToCart(prodNumber) {
-    let prodIndex=prodNumber-1;
-    let amountToAdd=parseInt($("#prod"+prodNumber+"Input").val());
-    if (amountToAdd+cartProducts[prodIndex]>Productos[prodIndex].stock)
+function addToCart(prodID) {
+    let prodIndex=Products.findIndex((product)=> {return product.id===prodID});
+    let amountToAdd=parseInt($(`#prod${prodID}Input`).val());
+    if (amountToAdd+Products[prodIndex].amountToBuy>Products[prodIndex].stock)
     {
-        alert('Lo sentimos, sólo tenemos disponibles '+Productos[prodIndex].stock+' unidades de este producto. Por favor, seleccione otra cantidad');
+        alert('Lo sentimos, sólo tenemos disponibles '+Products[prodIndex].stock+' unidades de este producto. Por favor, seleccione otra cantidad');
         return;
     }
-    cartProducts[prodIndex]+=amountToAdd;
+    Products[prodIndex].amountToBuy+=amountToAdd;
     setCart();
-
-    // if (cartProducts[prodIndex] == 0) {
-    //     cartNewRow(prodNumber, amountToAdd);
-    // }
-    // else {
-    //     cartProducts[prodIndex] += amountToAdd;
-    //     setCartAmount(prodNumber, cartProducts[prodIndex]);
-    // }
     setTotals();
 
-    // switch(prodNumber) {
-    //     case "1":
-    //         if (amountToAdd+cartProducts[0]>Productos[0].stock)
-    //         {
-    //             alert('Lo sentimos, sólo tenemos disponibles '+Productos[0].stock+' unidades de este producto. Por favor, seleccione otra cantidad');
-    //             break;
-    //         }
-
-    //         if (cartProducts[0] == 0) {
-    //             cartNewRow(1, amountToAdd);
-    //         }
-    //         else {
-    //             cartProducts[0] += amountToAdd;
-    //             setCartAmount("1", cartProducts[0]);
-    //         }
-    //     break;
-
-    //     case "2":
-    //         if (amountToAdd+cartProducts[1]>Productos[1].stock)
-    //         {
-    //             alert('Lo sentimos, sólo tenemos disponibles '+Productos[1].stock+' unidades de este producto. Por favor, seleccione otra cantidad');
-    //             break;
-    //         }
-
-    //         if (cartProducts[1] == 0) {
-    //             cartNewRow(2, amountToAdd);
-    //         }
-    //         else {
-    //             cartProducts[1] += amountToAdd;
-    //             setCartAmount("2", cartProducts[1]);
-    //         }
-    //     break;
-
-    //     case "3":
-    //         if (amountToAdd+cartProducts[2]>Productos[2].stock)
-    //         {
-    //             alert('Lo sentimos, sólo tenemos disponibles '+Productos[2].stock+' unidades de este producto. Por favor, seleccione otra cantidad');
-    //             break;
-    //         }
-    //         if (cartProducts[2] == 0) {
-    //             cartNewRow(3, amountToAdd);
-    //         }
-    //         else {
-    //             cartProducts[2] += amountToAdd;
-    //             setCartAmount("3", cartProducts[2]);
-    //         }
-    //     break;
-
-    //     default:
-    //     break;
-    // }
 }
 
-// function cartNewRow (prodNumber, amount) {
-
-//     let cartTable = $("#cartBody")[0];
-//     let prodIndex = prodNumber - 1;
-//     let prodString = Productos[prodIndex].name;
-
-//     cartProducts[prodIndex] += amount;
-    
-//     let newRow = cartTable.insertRow();
-    
-//     prodCell = newRow.insertCell();
-//     amoCell = newRow.insertCell();
-//     btnCell = newRow.insertCell();
-
-//     prodCell.className += "text-left";
-//     amoCell.className += "text-center";
-//     btnCell.className += "text-right";
-
-//     prodCell.appendChild(document.createTextNode(prodString));
-//     amoCell.appendChild(document.createTextNode(cartProducts[prodIndex]));
-    
-//     let deleteBtn = document.createElement("button");
-//     let icon = document.createElement("i");
-//     icon.className = "fa fa-trash";
-//     deleteBtn.className = "btn btn-sm btn-danger";
-//     deleteBtn.onclick = cartRemoveRow;
-//     deleteBtn.appendChild(icon);
-//     btnCell.appendChild(deleteBtn);
-
-// }
-
-function cartNewRow(producto) {
+function cartNewRow(product) {
     cartRowHTML =
-    `<tr>
-        <td class="text-left">${producto.name}</td>
-        <td class="text-center" id="prod${producto.id}Qty">0</td>
-        <td class="text-right"><button class="btn btn-sm btn-danger" onclick="removeFromCart(${producto.id})"><i class="fa fa-trash"></i></button></td>
+    `<tr id="prod${product.id}CartRow">
+        <td class="text-left">${product.name}</td>
+        <td class="text-center" id="prod${product.id}AmountToBuy">0</td>
+        <td class="text-right"><button class="btn btn-sm btn-danger" onclick="removeFromCart(${product.id})"><i class="fa fa-trash"></i></button></td>
     </tr>`;
 
     $("#cartBody").append(cartRowHTML).children().hide();
 }
 
 function setCart() {
-    for (producto of Productos)
+    for (product of Products)
     {
-        $("#prod"+producto.id+"Qty").text(cartProducts[producto.id-1]);
-        if (cartProducts[producto.id-1]==0) {
-            $("#cartBody").find('tr').eq(producto.id-1).hide();
+        $("#prod"+product.id+"AmountToBuy").text(product.amountToBuy);
+        if (product.amountToBuy==0) {
+            $(`#prod${product.id}CartRow`).hide();
         }
-        else $("#cartBody").find('tr').eq(producto.id-1).show();
+        else {
+            $(`#prod${product.id}AmountToBuy`).text(product.amountToBuy);
+            $(`#prod${product.id}CartRow`).show();
+        }
     }
 }
 
-// function setCartAmount (prodNumber, amount) {
-//     let cartTable = document.getElementById("cartBody");
-//     let n = cartTable.rows.length;
-//     let prodString = "Producto " + prodNumber;
 
-//     for (let r = 0; r < n; r++) {
-//         if (cartTable.rows[r].cells[0].innerHTML == prodString) {
-//             cartTable.rows[r].cells[1].innerHTML = amount;
-//         }
-//     }
-// }
-
-function removeFromCart(prodNumber) {
-    // let prodNumber = parseInt($(this).closest('tr').children().first().text().slice(9));
-    cartProducts[prodNumber-1]=0;
-    // $(this).closest('tr').remove();
+function removeFromCart(prodID) {
+    let prodIndex=Products.findIndex((product)=> {return product.id===prodID});
+    Products[prodIndex].amountToBuy=0;
     setCart();
     setTotals();
 }
 
 function setTotals() {
     let subtotal=0, shipCost, total;
-    for (let i=0;i<Productos.length;i++){
-        subtotal+=cartProducts[i]*Productos[i].price;
+    for (product of Products){
+        subtotal+=product.amountToBuy*product.price;
     }
     if (subtotal==0)
     {
@@ -229,4 +125,45 @@ function setTotals() {
 
 pesoFormat = (number) => {
     let numberPesos= new Intl.NumberFormat("es-419",{style:"currency",currency:"ARS",currencyDisplay:"narrowSymbol",minimumFractionDigits:"2"}).format(number);
-    return numberPesos;}
+    return numberPesos;
+}
+
+payButton = () => {
+    $.get(urlProducts)
+    .done((serverData)=>{
+        
+        let prodPurchase=[], serverDataIndex=0;
+        for (product of Products) {
+            if (product.amountToBuy==0) continue;
+            serverDataIndex=serverData.findIndex((dataProduct)=> {return dataProduct.id===product.id});
+            if (serverDataIndex==-1) {
+                alert(`El producto ${product.name} que intentaba comprar ha sido eliminado del catálogo. Recargaremos la página para mostrar los productos correctos`);
+                break;
+            }
+            else if (product.amountToBuy>serverData[serverDataIndex].stock) {
+                product.stock=serverData[serverDataIndex].stock;
+                product.amountToBuy=product.stock;
+                if (product.stock==0){
+                    $(`#prod${product.id}Stock`).text("Sin stock");
+                    $(`#prod${product.id}Input , #prod${product.id}AddButton`).prop("disabled",true);
+                }
+                alert(`Lo sentimos, quedan ${product.stock} unidades disponibles de ${product.name}`);
+                setCart();
+                setTotals();
+                return;
+
+            }
+            else prodPurchase.push(product);
+        }
+        if (serverDataIndex==-1) window.setTimeout(function(){window.location.reload()}, 3000);
+        else {
+            if (!prodPurchase.length) {
+                alert("Debe seleccionar al menos un producto");
+                return
+            }
+            sessionStorage.setItem("prodPurchase",JSON.stringify(prodPurchase));
+            window.location.href="imbrioscia_checkout.html";
+        }
+    })
+
+}
